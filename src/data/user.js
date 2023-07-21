40ec10e1afc82e713b672ab107493ed407cb20ee
macray <macraykorir254@gmail.com>
2023-07-21 13:31:27 +0300

    Imports and Exports

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,5 @@
-// src/data/user.js
-// eslint-disable-next-line
 const username = "Liza";
-// eslint-disable-next-line
 const city = "New York";
-
-// eslint-disable-next-line
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
-// export all three variables as named exports
 export { username, city, image };
