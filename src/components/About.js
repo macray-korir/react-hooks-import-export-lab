df075f0cd38094b11ab835afe60d58798f78813f
macray <macraykorir254@gmail.com>
2023-07-21 13:29:35 +0300

    Imports and Exports

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { image } from "../data/user"; // Import the image variable from user.js
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={image} alt="Profile" />
+      <p>Welcome to my portfolio site!</p>
     </div>
   );
 }
+
+export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,20 @@
+// src/components/NavBar.js
 import React from "react";
+
+const links = ["home", "about", "projects"]; // Array of strings for each link
 
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <ul>
+        {links.map((link, index) => (
+          <li key={index}>
+            <a href={`#${link}`}>{link}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
+
+export default NavBar;
